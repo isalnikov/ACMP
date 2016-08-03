32aea85ffea0847fce592764de05403fa76cbb8f
isalnikov <admin@192.168.1.2>
2016-08-04 00:23:23 +0300

    add task template

--- src/ru/isalnikov/acmp/acmp2/Main.java
+++ src/ru/isalnikov/acmp/acmp2/Main.java
@@ -1,24 +1,26 @@
-package ru.isalnikov.acmp.acmp1;
+package ru.isalnikov.acmp.acmp2;
 
+/**
+ * Требуется посчитать сумму целых чисел от 1 до N.
+ */
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
         try (Scanner in = new Scanner(System.in);
                 PrintWriter out = new PrintWriter(System.out)) {
-
             solve(in, out);
         }
     }
 
- 
     private static void solve(Scanner in, PrintWriter out) {
-        int a = in.nextInt();
-        int b = in.nextInt();
-        out.println(a + b);
+        int n = in.nextInt();
+        if (n > 0) {
+            out.println((n * (n + 1)) / 2);
+        } else {
+            out.println((Math.abs(n) * (-1 + n)) / 2 +1);
+        }
     }
-
 }
