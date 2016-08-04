d5742f5e033651d52be1b112281c29bb694b1ac7
Igor Salnikov <isalnikov1@gmail.com>
2016-08-04 18:53:42 +0300

    add tasks 8 + unit test

--- src/test/java/ru/isalnikov/acmp/acmp8/MainTest.java
+++ src/test/java/ru/isalnikov/acmp/acmp8/MainTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ru.isalnikov.acmp.acmp7;
+package ru.isalnikov.acmp.acmp8;
 
 import java.io.ByteArrayInputStream;
 import org.junit.Test;
@@ -21,38 +21,28 @@ public class MainTest extends BaseTest {
 
     @Test
     public void test1() {
-        String data = "5 7 3";
+        String data = "8 54 432";
 
         System.setIn(new ByteArrayInputStream(data.getBytes()));
 
         Main.main(null);
 
-        assertEquals("7", outContent.toString());
+        assertEquals("YES", outContent.toString());
 
     }
 
     @Test
     public void test2() {
-        String data = "987531 234 86364";
+        String data = "16 19 777";
 
         System.setIn(new ByteArrayInputStream(data.getBytes()));
 
         Main.main(null);
 
-        assertEquals("987531", outContent.toString());
+        assertEquals("NO", outContent.toString());
 
     }
 
-    @Test
-    public void test3() {
-        String data = "189285 283 4958439238923098349024";
 
-        System.setIn(new ByteArrayInputStream(data.getBytes()));
-
-        Main.main(null);
-
-        assertEquals("4958439238923098349024", outContent.toString());
-
-    }
 
 }
