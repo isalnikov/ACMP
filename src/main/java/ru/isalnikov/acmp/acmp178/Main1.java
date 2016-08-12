0ca82e9623efbd9b9d8dcceae95124bdf40c10f5
Igor Salnikov <isalnikov1@gmail.com>
2016-08-12 17:21:19 +0300

    add tasks 178 unit test

--- src/main/java/ru/isalnikov/acmp/acmp178/Main1.java
+++ src/main/java/ru/isalnikov/acmp/acmp178/Main1.java
@@ -23,189 +23,191 @@ public class Main1 {
 
     public static class IntHashMap {
 
-    private transient Entry table[];
+        private transient Entry table[];
 
-    private transient int count;
+        private transient int count;
 
-    private int threshold;
+        private int threshold;
 
-    private float loadFactor;
+        private float loadFactor;
 
-    Entry sortByValue() {
-        rehash();
-        Arrays.sort(table, (Entry o1, Entry o2) -> {
-            
-            if( o1 != null && o2 == null) return -1;
-            if( o1 == null && o2 == null) return 0;
-            if( o1 == null && o2 != null) return 1;
-            
-            int cmp = Integer.compare(o2.value, o1.value);
-            if (cmp == 0) {
-                cmp = Integer.compare(o1.key, o2.key);
+        Entry sortByValue() {
+            rehash();
+            Arrays.sort(table, (Entry o1, Entry o2) -> {
+
+                if (o1 != null && o2 == null) {
+                    return -1;
+                }
+                if (o1 == null && o2 == null) {
+                    return 0;
+                }
+                if (o1 == null && o2 != null) {
+                    return 1;
+                }
+
+                int cmp = Integer.compare(o2.value, o1.value);
+                if (cmp == 0) {
+                    cmp = Integer.compare(o1.key, o2.key);
+                }
+                return cmp;
+            });
+
+            return table[0];
+        }
+
+        public static class Entry {
+
+            int hash;
+            int key;
+            int value;
+            Entry next;
+
+            protected Entry(int hash, int key, int value, Entry next) {
+                this.hash = hash;
+                this.key = key;
+                this.value = value;
+                this.next = next;
             }
-            return cmp;
-        });
-        
-        return table[0];
-    }
 
-    public static class Entry {
-
-        int hash;
-        int key;
-        int value;
-        Entry next;
-
-        protected Entry(int hash, int key, int value, Entry next) {
-            this.hash = hash;
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
-        
-    }
-
-    public IntHashMap() {
-        this(20, 0.75f);
-    }
-
-    public IntHashMap(int initialCapacity) {
-        this(initialCapacity, 0.75f);
-    }
-
-    public IntHashMap(int initialCapacity, float loadFactor) {
-        super();
-        if (initialCapacity < 0) {
-            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
-        }
-        if (loadFactor <= 0) {
-            throw new IllegalArgumentException("Illegal Load: " + loadFactor);
-        }
-        if (initialCapacity == 0) {
-            initialCapacity = 1;
         }
 
-        this.loadFactor = loadFactor;
-        table = new Entry[initialCapacity];
-        threshold = (int) (initialCapacity * loadFactor);
-    }
+        public IntHashMap() {
+            this(20, 0.75f);
+        }
 
-    public int size() {
-        return count;
-    }
+        public IntHashMap(int initialCapacity) {
+            this(initialCapacity, 0.75f);
+        }
 
-    public boolean isEmpty() {
-        return count == 0;
-    }
+        public IntHashMap(int initialCapacity, float loadFactor) {
+            super();
+            if (initialCapacity < 0) {
+                throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+            }
+            if (loadFactor <= 0) {
+                throw new IllegalArgumentException("Illegal Load: " + loadFactor);
+            }
+            if (initialCapacity == 0) {
+                initialCapacity = 1;
+            }
 
-    public boolean contains(int value) {
+            this.loadFactor = loadFactor;
+            table = new Entry[initialCapacity];
+            threshold = (int) (initialCapacity * loadFactor);
+        }
 
-        Entry tab[] = table;
-        for (int i = tab.length; i-- > 0;) {
-            for (Entry e = tab[i]; e != null; e = e.next) {
-                if (e.value == value) {
+        public int size() {
+            return count;
+        }
+
+        public boolean isEmpty() {
+            return count == 0;
+        }
+
+        public boolean contains(int value) {
+
+            Entry tab[] = table;
+            for (int i = tab.length; i-- > 0;) {
+                for (Entry e = tab[i]; e != null; e = e.next) {
+                    if (e.value == value) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public boolean containsValue(int value) {
+            return contains(value);
+        }
+
+        public boolean containsKey(int key) {
+            Entry tab[] = table;
+            int hash = key;
+            int index = (hash & 0x7FFFFFFF) % tab.length;
+            for (Entry e = tab[index]; e != null; e = e.next) {
+                if (e.hash == hash) {
                     return true;
                 }
             }
+            return false;
         }
-        return false;
-    }
 
-    public boolean containsValue(int value) {
-        return contains(value);
-    }
-
-    public boolean containsKey(int key) {
-        Entry tab[] = table;
-        int hash = key;
-        int index = (hash & 0x7FFFFFFF) % tab.length;
-        for (Entry e = tab[index]; e != null; e = e.next) {
-            if (e.hash == hash) {
-                return true;
+        public int get(int key) {
+            Entry tab[] = table;
+            int hash = key;
+            int index = (hash & 0x7FFFFFFF) % tab.length;
+            for (Entry e = tab[index]; e != null; e = e.next) {
+                if (e.hash == hash) {
+                    return e.value;
+                }
             }
+            return Integer.MIN_VALUE;
         }
-        return false;
-    }
 
-    public int get(int key) {
-        Entry tab[] = table;
-        int hash = key;
-        int index = (hash & 0x7FFFFFFF) % tab.length;
-        for (Entry e = tab[index]; e != null; e = e.next) {
-            if (e.hash == hash) {
-                return e.value;
-            }
+        protected void vacuum() {
+
         }
-        return Integer.MIN_VALUE;
-    }
 
-    protected void vacuum(){
-        
-    }
-    
+        protected void rehash() {
+            int oldCapacity = table.length;
+            Entry oldMap[] = table;
 
-    
-    protected void rehash() {
-        int oldCapacity = table.length;
-        Entry oldMap[] = table;
+            int newCapacity = oldCapacity * 2 + 1;
+            Entry newMap[] = new Entry[newCapacity];
 
-        int newCapacity = oldCapacity * 2 + 1;
-        Entry newMap[] = new Entry[newCapacity];
+            threshold = (int) (newCapacity * loadFactor);
+            table = newMap;
 
-        threshold = (int) (newCapacity * loadFactor);
-        table = newMap;
+            for (int i = oldCapacity; i-- > 0;) {
+                for (Entry old = oldMap[i]; old != null;) {
+                    Entry e = old;
+                    old = old.next;
 
-        for (int i = oldCapacity; i-- > 0;) {
-            for (Entry old = oldMap[i]; old != null;) {
-                Entry e = old;
-                old = old.next;
-
-                int index = (e.hash & 0x7FFFFFFF) % newCapacity;
-                e.next = newMap[index];
-                newMap[index] = e;
-            }
-        }
-    }
-
-    public int put(int key, int value) {
-
-        Entry tab[] = table;
-        int hash = key;
-        int index = (hash & 0x7FFFFFFF) % tab.length;
-        for (Entry e = tab[index]; e != null; e = e.next) {
-            if (e.hash == hash) {
-                int old = e.value;
-                e.value = value;
-                return old;
+                    int index = (e.hash & 0x7FFFFFFF) % newCapacity;
+                    e.next = newMap[index];
+                    newMap[index] = e;
+                }
             }
         }
 
-        if (count >= threshold) {
-            // Rehash the table if the threshold is exceeded
-            rehash();
+        public int put(int key, int value) {
 
-            tab = table;
-            index = (hash & 0x7FFFFFFF) % tab.length;
+            Entry tab[] = table;
+            int hash = key;
+            int index = (hash & 0x7FFFFFFF) % tab.length;
+            for (Entry e = tab[index]; e != null; e = e.next) {
+                if (e.hash == hash) {
+                    int old = e.value;
+                    e.value = value;
+                    return old;
+                }
+            }
+
+            if (count >= threshold) {
+                // Rehash the table if the threshold is exceeded
+                rehash();
+
+                tab = table;
+                index = (hash & 0x7FFFFFFF) % tab.length;
+            }
+
+            // Creates the new entry.
+            Entry e = new Entry(hash, key, value, tab[index]);
+            tab[index] = e;
+            count++;
+            return Integer.MIN_VALUE;
         }
 
-        // Creates the new entry.
-        Entry e = new Entry(hash, key, value, tab[index]);
-        tab[index] = e;
-        count++;
-        return Integer.MIN_VALUE;
-    }
-
-        public  void clear() {
-        Entry tab[] = table;
-        for (int index = tab.length; --index >= 0;) {
-            tab[index] = null;
+        public void clear() {
+            Entry tab[] = table;
+            for (int index = tab.length; --index >= 0;) {
+                tab[index] = null;
+            }
+            count = 0;
         }
-        count = 0;
     }
-}
 
-    
-    
     public static void main(String[] args) {
 
         try (Scanner in = new Scanner(System.in);
@@ -223,49 +225,44 @@ public class Main1 {
         System.arraycopy(b, 0, c, aLen, bLen);
         return c;
     }
-    
+
     private static void solve(Scanner in, PrintWriter out) {
         int N = in.nextInt();
         int array[] = new int[N];
 
-        IntHashMap hashMap = new IntHashMap(N/3);
+        IntHashMap hashMap = new IntHashMap();
 
         int count = 0;
         for (int i = 0; i < N; i++) {
             int next = in.nextInt();
             array[i] = next;
-             count = 1;
-            if(hashMap.containsKey(next)){
-                count = 1+ hashMap.get(next);
+            count = 1;
+            if (hashMap.containsKey(next)) {
+                count = 1 + hashMap.get(next);
             }
             hashMap.put(next, count);
         }
-   
-        
+
         IntHashMap.Entry max = hashMap.sortByValue();
         int maxKey = max.key;
         int maxValue = max.value;
         hashMap.clear();
         hashMap = null;
-        int [] fill  = new int[maxValue];
+        int[] fill = new int[maxValue];
         Arrays.fill(fill, maxKey);
 
-        
-         //IntStream maxIntStream = IntStream.generate(()->max.key).limit(max.value);
-
         IntStream intStream = Arrays.stream(array);
-        int[] result = concat(intStream.filter(i -> i != maxKey).toArray() , fill);
-        // IntStream result = IntStream.concat(intStream.filter(i -> i != max.key), maxIntStream);
-    
-        StringBuffer sb = new StringBuffer();
+        int[] result = concat(intStream.filter(i -> i != maxKey).toArray(), fill);
+
+        boolean isNull = false;
         for (int i = 0; i < result.length; i++) {
-            if (sb.length() > 0) {
-                sb.append(" ");
+            if (isNull) {
+                out.print(' ');
             }
-            sb.append(result[i]);
+            isNull = true;
+            out.print(result[i]);
         }
-       
-        out.print(sb);
+
         out.flush();
     }
 
