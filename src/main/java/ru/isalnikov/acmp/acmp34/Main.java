d80d74367abbf92df35f55cffe1c7c838a6d595c
Igor Salnikov <isalnikov1@gmail.com>
2016-08-19 11:35:15 +0300

    add tasks 34 unit test

--- src/main/java/ru/isalnikov/acmp/acmp34/Main.java
+++ src/main/java/ru/isalnikov/acmp/acmp34/Main.java
@@ -17,6 +17,8 @@ package ru.isalnikov.acmp.acmp34;
  * 105) и k (1 ≤ k ≤ 5) – длину шифровки и длину номера секретной базы
  * соответственно. Вторая строка содержит n цифр – шифровку. Помните, что цифры
  * в шифровке не разделяются пробелами.
+ *
+ *
  */
 import java.io.*;
 import java.util.*;
@@ -26,36 +28,23 @@ class Main {
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in);
                 PrintWriter out = new PrintWriter(System.out)) {
-            solve(in, out);
-        }
-    }
+            int n = in.nextInt();
+            int k = in.nextInt();
+            String s = in.next();
+            int[] array = new int[(int) Math.pow(10, k)];
+            for (int i = 0; i < n - k; i++) {
+                Integer key = Integer.parseInt(s.substring(i, k + i));
+                array[key]++;
+                if (array[key] > 1) {
+                    out.print("YES");
+                    out.flush();
+                    return;
+                }
 
-    private static enum Result {
-        YES, NO;
-    }
-
-    private static void solve(Scanner in, PrintWriter out) {
-        int n = in.nextInt();
-        int k = in.nextInt();
-        in.nextLine();
-        String s = in.nextLine();
-        int [] array =  new int[100_000];
-
-        for (int i = 0; i < n - k; i++) {
-            Integer key = Integer.parseInt(s.substring(i, k + i));
-
-            array[key]++;
-
-            if (array[key] > 1) {
-                out.print(Result.YES);
-                out.flush();
-                return;
             }
-
+            out.print("NO");
+            out.flush();
         }
-
-        out.print(Result.NO);
-        out.flush();
     }
 
 }
--- src/test/java/ru/isalnikov/acmp/acmp34/MainTest.java
+++ src/test/java/ru/isalnikov/acmp/acmp34/MainTest.java
@@ -1,15 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Тут линейная сложность возникает из-за малых ограничений на k. Если бы k было, скажем, 10 или больше, единственным выходом были бы хеши. А так можно додуматься, что код на самом деле-это какое-то число(минимум 0, максимум 99999). Заводим массив на 100к элементов, проходимся по входной строке и запоминаем кол-во чисел(полученных из всех подстрок длины k). Дальше всё тривиально, за один проход можем сказать да или нет.
  */
 package ru.isalnikov.acmp.acmp34;
 
 import java.io.ByteArrayInputStream;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import ru.isalnikov.acmp.base.BaseTest;
