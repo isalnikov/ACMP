e18427bde88eba45f09ee057938961117ae8d970
Igor Salnikov <isalnikov1@gmail.com>
2016-08-24 13:31:45 +0300

    add tasks 40

---
+++ src/main/java/ru/isalnikov/acmp/acmp39/Main.java
@@ -0,0 +1,27 @@
+package ru.isalnikov.acmp.acmp39;
+
+/**
+ * Необходимо вычислить значение 2n.
+ *
+ * Входные данные
+ *
+ * В единственной строке входного файла INPUT.TXT записано натуральное число n
+ * (0 < n < 1000).
+ *
+ *
+ */
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in);
+                PrintWriter out = new PrintWriter(System.out)) {
+            int n = in.nextInt();
+            out.print(BigInteger.valueOf(2).pow(n));
+            out.flush();
+        }
+    }
+
+}
---
+++ src/test/java/ru/isalnikov/acmp/acmp39/MainTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.isalnikov.acmp.acmp39;
+
+import java.io.ByteArrayInputStream;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+import ru.isalnikov.acmp.base.BaseTest;
+
+public class MainTest extends BaseTest {
+
+    @Test
+    public void test() {
+        String data = "3";
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        Main.main(null);
+
+        assertEquals("8", outContent.toString());
+
+    }
+
+    @Test
+    public void test1() {
+        String data = "10";
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        Main.main(null);
+
+        assertEquals("1024", outContent.toString());
+
+    }
+
+    @Test
+    public void test2() {
+        String data = "72";
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        Main.main(null);
+
+        assertEquals("4722366482869645213696", outContent.toString());
+
+    }
+
+    @Test
+    @Ignore
+    public void test3() {
+        String data = "1000";
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        Main.main(null);
+
+        assertEquals("1071508607186267320948425049060001810561404...", outContent.toString());
+
+    }
+
+}
