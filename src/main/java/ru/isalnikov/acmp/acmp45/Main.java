036b1023fe10679b5bae10540f22b0a230d606dc
Igor Salnikov <isalnikov1@gmail.com>
2016-09-01 17:56:30 +0300

    add tasks 45

--- src/main/java/ru/isalnikov/acmp/acmp45/Main.java
+++ src/main/java/ru/isalnikov/acmp/acmp45/Main.java
@@ -30,67 +30,72 @@ public class Main {
         int n = in.nextInt();
 
         String res = "";
-       
 
-            if (n == 0) {
-                res = "10";
-            }
-            if (n < 10) {
-                res = "" + n;
-            }
+        if (n == 0) {
+            res = "10";
+            out.print(res);
 
-            int[] ar = new int[10];
-            for (int i = 2; i <= 9; i++) {
-                ar[i] = 0;
-            }
-            for (int i = 2; i <= 9; i++) {
-                while (n % i == 0) {
-                    n /= i;
-                    ar[i]++;
-                }
-            }
-            if (n != 1) {
-                res = "-1";
-            }
-            while (ar[2] >= 3) {
-                ar[2] -= 3;
-                ar[8]++;
-            }
-            while (ar[3] >= 2) {
-                ar[3] -= 2;
-                ar[9]++;
-            }
-            while ((ar[2] > 0) && (ar[4] > 0)) {
-                ar[2]--;
-                ar[4]--;
-                ar[8]++;
-            }
-            while ((ar[2] > 0) && (ar[3] > 0)) {
-                ar[2]--;
-                ar[3]--;
-                ar[6]++;
-            }
-            while (ar[2] >= 2) {
-                ar[2] -= 2;
-                ar[4]++;
-            }
-            while ((ar[2] > 0) && (ar[4] > 0)) {
-                ar[2]--;
-                ar[4]--;
-                ar[8]++;
-            }
-            for (int i = 2; i <= 9; i++) {
-                while (ar[i] > 0) {
-                    ar[i]--;
-                    res += i;
-                }
-            }
-            if (res.equals("")) {
-                res = "-1";
+            out.flush();
+            return;
+        }
+        if (n < 10) {
+            res = "" + n;
+            out.print(res);
 
+            out.flush();
+            return;
+        }
+
+        int[] ar = new int[10];
+        for (int i = 2; i <= 9; i++) {
+            ar[i] = 0;
+        }
+        for (int i = 2; i <= 9; i++) {
+            while (n % i == 0) {
+                n /= i;
+                ar[i]++;
             }
+        }
+        if (n != 1) {
+            res = "-1";
+        }
+        while (ar[2] >= 3) {
+            ar[2] -= 3;
+            ar[8]++;
+        }
+        while (ar[3] >= 2) {
+            ar[3] -= 2;
+            ar[9]++;
+        }
+        while ((ar[2] > 0) && (ar[4] > 0)) {
+            ar[2]--;
+            ar[4]--;
+            ar[8]++;
+        }
+        while ((ar[2] > 0) && (ar[3] > 0)) {
+            ar[2]--;
+            ar[3]--;
+            ar[6]++;
+        }
+        while (ar[2] >= 2) {
+            ar[2] -= 2;
+            ar[4]++;
+        }
+        while ((ar[2] > 0) && (ar[4] > 0)) {
+            ar[2]--;
+            ar[4]--;
+            ar[8]++;
+        }
+        for (int i = 2; i <= 9; i++) {
+            while (ar[i] > 0) {
+                ar[i]--;
+                res += i;
+            }
+        }
+        if (res.equals("")) {
+            res = "-1";
 
-
+        }
 
         out.print(res);
 
--- src/test/java/ru/isalnikov/acmp/acmp45/MainTest.java
+++ src/test/java/ru/isalnikov/acmp/acmp45/MainTest.java
@@ -46,5 +46,61 @@ public class MainTest extends BaseTest {
         assertEquals("259", outContent.toString());
 
     }
+    @Test
+    public void test6() {
+        String data = "24";
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        Main.main(null);
+
+        assertEquals("38", outContent.toString());
+
+    }
+    @Test
+    
+    public void test7() {
+        String data = "2048";
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        Main.main(null);
+
+        assertEquals("4888", outContent.toString()); //22222222222
+
+    }
+    @Test
+    public void test8() {
+        String data = "9765625";
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        Main.main(null);
+
+        assertEquals("5555555555", outContent.toString());
+
+    }
+    @Test
+    public void test9() {
+        String data = "976562500";
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        Main.main(null);
+
+        assertEquals("4555555555555", outContent.toString());
+
+    }
+    @Test
+    public void test10() {
+        String data = "13824";
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        Main.main(null);
+
+        assertEquals("38889", outContent.toString());
+
+    }
 
 }
