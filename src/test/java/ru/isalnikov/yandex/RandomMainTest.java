24ec1241706cc7cd274bde34cc3c9c951cce238e
isalnikov <admin@192.168.1.2>
2016-10-04 22:20:47 +0300

    add yandex rand2

---
+++ src/test/java/ru/isalnikov/yandex/RandomMainTest.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.isalnikov.yandex;
+
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Igor Salnikov <admin@isalnikov.com>
+ */
+public class RandomMainTest {
+
+    private final static double EPSILON = 0.01;
+
+    public static boolean equals(double a, double b) {
+        return a == b ? true : Math.abs(a - b) < EPSILON;
+    }
+
+    public RandomMainTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testRand2() {
+        RandomMain rm = new RandomMain();
+        int count = 100_000_000;
+        HashMap<Integer, AtomicInteger> map = new HashMap<>();
+        for (int i = 0; i < count; i++) {
+            int next = rm.rand2();
+
+            map.putIfAbsent(next, new AtomicInteger(0));
+            map.get(next).incrementAndGet();
+
+        }
+
+        System.out.println(map);
+        double value = 2.0 * (map.get(0).get()) / (count);
+        System.out.println(value);
+        assertTrue(equals(value, 1.0));
+
+    }
+
+    /**
+     * Test of rand3 method, of class RandomMain.
+     */
+    @Test
+    public void testRand3() {
+        System.out.println("rand3");
+        RandomMain instance = new RandomMain();
+        int expResult = 0;
+        int result = instance.rand3();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of rand3_1 method, of class RandomMain.
+     */
+    @Test
+    public void testRand3_1() {
+
+        RandomMain rm = new RandomMain();
+        int count = 100_000_000;
+        HashMap<Integer, AtomicInteger> map = new HashMap<>();
+        for (int i = 0; i < count; i++) {
+            int next = rm.rand3_1();
+
+            map.putIfAbsent(next, new AtomicInteger(0));
+            map.get(next).incrementAndGet();
+
+        }
+
+        System.out.println(map);
+        double value = 3.0 * (map.get(0).get()) / (count);
+        System.out.println(value);
+        assertTrue(equals(value, 1.0));
+
+    }
+
+}
