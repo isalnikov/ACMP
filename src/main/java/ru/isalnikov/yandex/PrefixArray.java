6cd43bc29000f58af088f433ca62993210aa8734
Igor Salnikov <isalnikov1@gmail.com>
2016-10-05 09:52:58 +0300

    Stack sort

---
+++ src/main/java/ru/isalnikov/yandex/PrefixArray.java
@@ -0,0 +1,19 @@
+
+package ru.isalnikov.yandex;
+
+/**
+ * 2. Есть два односвязных массива, у которых есть общая часть. Нужно выяснить
+ * индексы массивов, с которых начинается общая часть. Примеры массивов:
+ *
+ * - a b c x y z
+ * - d e a d b e e f x y z
+ *
+  */
+public class PrefixArray {
+
+  
+    public static void main(String[] args) {
+  
+    }
+
+}
---
+++ src/main/java/ru/isalnikov/yandex/StackSort.java
@@ -0,0 +1,38 @@
+
+package ru.isalnikov.yandex;
+
+import java.util.Stack;
+
+
+public class StackSort {
+
+    public static void main(String[] args) {
+        StackSort stackSort = new StackSort();
+        int[] data = {5, 2, 1, 9, 0, 10};
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < data.length; i++) {
+            stack.push(data[i]);
+        }
+        System.out.println(stack);
+        stackSort.sort(stack);
+    }
+
+    public void sort(Stack s) {
+        Stack<Integer> temp = new Stack<>();
+
+        while (!s.isEmpty()) {
+
+            int s1 = (int) s.pop();
+
+            while (!temp.isEmpty() && (temp.peek() > s1)) {
+                s.push(temp.pop());
+            }
+            temp.push(s1);
+
+        }
+
+        System.out.println(temp);
+
+    }
+}
