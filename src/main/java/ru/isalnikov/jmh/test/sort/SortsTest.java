69da9eae7de236b8fdd8f7e174cf6a9e3e117eec
Igor Salnikov <isalnikov1@gmail.com>
2016-11-11 16:33:24 +0300

    360

--- src/main/java/ru/isalnikov/jmh/test/sort/SortsTest.java
+++ src/main/java/ru/isalnikov/jmh/test/sort/SortsTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ru.isalnikov.tests.sorts;
+package ru.isalnikov.jmh.test.sort;
 
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
