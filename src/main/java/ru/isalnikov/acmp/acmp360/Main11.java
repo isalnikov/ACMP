b971570534bc9a0304556173a52b7a349ed13555
Igor Salnikov <isalnikov1@gmail.com>
2016-11-11 10:15:42 +0300

    360

--- src/main/java/ru/isalnikov/acmp/acmp360/Main11.java
+++ src/main/java/ru/isalnikov/acmp/acmp360/Main11.java
@@ -18,61 +18,38 @@ import java.io.*;
 import java.util.*;
 
 class Main11 {
-   
-   
-    
-    
+
     public static void main(String[] args) {
-      
         try (BufferedReader in = new BufferedReader(new FileReader("input.txt"));
                 PrintWriter out = new PrintWriter(System.out)) {
-            solve(in, out);
-        } catch (Exception e) {
-        }
-    
-    }
+            int n = Integer.parseInt(in.readLine());
+            int max = Integer.MIN_VALUE;
+            int[] a = new int[n];
+            int[] b = new int[n];
+            int[] c = new int[n];
 
-    private static void solve(BufferedReader in, PrintWriter out) throws IOException {
-        int n = Integer.parseInt(in.readLine());
-        
-        int max = Integer.MIN_VALUE;
-        int[] a = new int[n]; // <- 1 <-4
-        int[] b = new int[n]; // <- 2 
-        int[] c = new int[n]; // <- 3
-        
-        for (int i = 0; i < n; i++) {
-
-            int[] t = a;
-            a = b;
-            b = c;
-            c = t;
-
-            String s = in.readLine();
-            c = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).toArray();
-            //    c = Arrays.asList(s.split(" ")).stream().mapToInt(Integer::parseInt).toArray();
-
-            for (int j = 0; j < n; j++) {
-                
-                if((j + 2)  < n){
-                   max = Math.max(max, c[j] + c[j + 1] + c[j + 2]);
-                }
-                
-                if (i >= 1 && ((j + 1 ) < n)) {
-                    
-                    max = Math.max(max, b[j] + b[j + 1] + c[j + 1]);
-                    max = Math.max(max, b[j] + b[j + 1] + c[j]);
-                    max = Math.max(max, c[j] + c[j + 1] + b[j + 1]);
-                    max = Math.max(max, c[j] + c[j + 1] + b[j]);
-                }
-  
-                if (i >= 2) {
-                    max = Math.max(max, a[j] + b[j] + c[j]);
-
+            for (int i = 0; i < n; i++) {
+                int[] t = a; a = b; b = c; c = t; String s = in.readLine();
+                c = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).toArray();
+                for (int j = 0; j < n; j++) {
+                    if ((j + 2) < n) {
+                        max = Math.max(max, c[j] + c[j + 1] + c[j + 2]);
+                    }
+                    if (i >= 1 && ((j + 1) < n)) {
+                        max = Math.max(max, b[j] + b[j + 1] + c[j + 1]);
+                        max = Math.max(max, b[j] + b[j + 1] + c[j]);
+                        max = Math.max(max, c[j] + c[j + 1] + b[j + 1]);
+                        max = Math.max(max, c[j] + c[j + 1] + b[j]);
+                    }
+                    if (i >= 2) {
+                        max = Math.max(max, a[j] + b[j] + c[j]);
+                    }
                 }
             }
+            out.print(max);
+            out.flush();
+        } catch (Exception e) {
         }
-        out.print(max);
-        out.flush();
     }
 
 }
