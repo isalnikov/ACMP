a8a0a026b07966c6a5c88a839847a0703b5514a9
Igor Salnikov <isalnikov1@gmail.com>
2016-11-17 11:51:32 +0300

    360

--- src/main/java/ru/isalnikov/yandex/BalanceProblem.java
+++ src/main/java/ru/isalnikov/yandex/BalanceProblem.java
@@ -1,13 +1,18 @@
 package ru.isalnikov.yandex;
 
+import java.math.BigDecimal;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * http://vpros.ru/27940-yava-sinkhronizatsii-atomarno-perevoda-deneg-za-s
+ * https://www.securecoding.cert.org/confluence/display/java/LCK07-J.+Avoid+deadlock+by+requesting+and+releasing+locks+in+the+same+order
+ */
 public class BalanceProblem {
 
-  static class Account {
+    static class Account {
 
         private int balance;
 
@@ -63,29 +68,40 @@ public class BalanceProblem {
 
         Account from = new Account();
         from.setBalance(10000);
-        
+
         Account to = new Account();
         to.setBalance(10000);
-        
-        
+
         ExecutorService service = Executors.newFixedThreadPool(10);
         AtomicInteger integer = new AtomicInteger(0);
         for (int i = 0; i < 1000; i++) {
-         service.execute(()->{
-            final  int  amount = 1;
-             System.out.println(integer.incrementAndGet() + " " + Thread.currentThread().getName() + " amount " + amount  +" from : "+ from.getBalance() + " to :"+to.getBalance() );
-             transfer(from, to, amount);
-         });
-            
+            service.execute(() -> {
+                final int amount = 1;
+                System.out.println(integer.incrementAndGet() + " " + Thread.currentThread().getName() + " amount " + amount + " from : " + from.getBalance() + " to :" + to.getBalance());
+                transfer(from, to, amount);
+            });
+
         }
-        
- 
+
         service.awaitTermination(5, TimeUnit.SECONDS);
         service.shutdown();
-        
-        System.out.println("from "+from.getBalance());
-        System.out.println("to   "+to.getBalance());
-        
+
+        System.out.println("from " + from.getBalance());
+        System.out.println("to   " + to.getBalance());
+
     }
 
+//    
+//public void transfer(Account acc1, Account acc2, BigDecimal value) {
+// 
+//    Object lock1 = System.identityHashCode(acc1) < System.identityHashCode(acc2) ? acc1.LOCK : acc2.LOCK;
+//    Object lock2 = System.identityHashCode(acc1) < System.identityHashCode(acc2) ? acc2.LOCK : acc1.LOCK;
+//    synchronized (lock1) {
+//       synchronized (lock2) {
+//          acc1.widrawal(value);
+//          acc2.send(value);
+//       }
+//    }
+//}
+    
 }
--- src/main/java/ru/isalnikov/yandex/BannerSolution.java
+++ src/main/java/ru/isalnikov/yandex/BannerSolution.java
@@ -3,24 +3,61 @@
  */
 package ru.isalnikov.yandex;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Random;
+import java.util.TreeSet;
 
 public class BannerSolution {
 
     public static void main(String[] args) {
+
+        linkedListSolution();
+        //arraySolution();
+    }
+
+    public static void arraySolution() {
         Random r = new Random();
-        int[] list = new int[100];// в угоду наглядности в дебри линкедлистов лезть не будем
-        for (int i = 0; i < 100; ++i) {
+        int size = 100;
+        int[] list = new int[size];// в угоду наглядности в дебри линкедлистов лезть не будем
+        for (int i = 0; i < size; ++i) {
             list[i] = i;
         }
+        //System.out.println(Arrays.toString(list));
         /*логика алгоритма*/
-        int index = 99;
+        int index = size ;
         int number;
+        
+        while (index > 0) {
+            number = r.nextInt(index) ;
+            --index;
+            int n = list[number];
+            System.out.print(String.format(" %d",n));
+            list[number] = list[index];
+        }
+    }
+
+    public static void linkedListSolution() {
+        LinkedList<Integer> list = new LinkedList();
+        Random r = new Random();
+         int index = 100;
+
+        for (int i = 0; i < index; ++i) {
+            list.add(i);
+        }
+
+        System.out.println(list);
+        /*логика алгоритма*/
+        int number;
+        TreeSet<Integer> treeSet = new TreeSet<>();
         while (index > 0) {
             number = r.nextInt(index);
-            System.out.println(list[number]);
-            list[number] = list[index];
             --index;
+            treeSet.add(list.get(number));
+            list.set(number, list.get(index));
+          
         }
+        System.out.println("");
+        System.out.println(treeSet);
     }
 }
