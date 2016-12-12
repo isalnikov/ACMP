9b1270167639719a61b4f117d764cf5069bc3c1c
Igor Salnikov <isalnikov1@gmail.com>
2016-12-12 15:47:56 +0300

    ЗАДАЧА №86

---
+++ src/test/java/ru/isalnikov/acmp/acmp86/MainTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.isalnikov.acmp.acmp86;
+
+import java.io.ByteArrayInputStream;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import ru.isalnikov.acmp.base.BaseTest;
+
+/**
+ *
+ * @author Igor Salnikov  <igor.salnikov@stoloto.ru>
+ */
+public class MainTest extends BaseTest {
+
+
+    @Test
+    public void test1() {
+        String data = "3";
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        Main.main(null);
+
+        assertEquals("2", outContent.toString());
+
+    }
+}
