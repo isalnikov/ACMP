9c2e790597652dce6715d925be85da7fc742e470
Igor Salnikov <isalnikov1@gmail.com>
2017-05-04 18:09:20 +0300

    add pattern

--- src/main/java/ru/isalnikov/sportloto/pattern/RequestHandlerImpl.java
+++ src/main/java/ru/isalnikov/sportloto/pattern/RequestHandlerImpl.java
@@ -3,7 +3,7 @@ package ru.isalnikov.sportloto.pattern;
 public class RequestHandlerImpl implements RequestHandler<Request, Response> {
 
     @Override
-    public Response handleRequest(Request request, Context context) { //Context - lamda
+    public Response handleRequest(Request request, Context context) { //Context - lambda
         String greeting = String.format("Hello %s", request.toString());
         return new Response(greeting);
 
