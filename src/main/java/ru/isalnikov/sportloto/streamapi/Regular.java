5446d5efa927597f1e22daa41dbd5023930f9154
Igor Salnikov <isalnikov1@gmail.com>
2017-11-16 17:19:17 +0300

    add

---
+++ src/main/java/ru/isalnikov/sportloto/streamapi/Regular.java
@@ -0,0 +1,17 @@
+package ru.isalnikov.sportloto.streamapi;
+
+/**
+ *
+ * @author Igor Salnikov igor.salnikov@stoloto.ru
+ */
+public class Regular {
+    
+    public static void main(String[] args) {
+        System.out.println("start");
+        long start =  System.currentTimeMillis();
+        String st = "12345".replaceAll("((((.*)*)*)*)*!","");
+        System.out.println(String.format("%s ms",System.currentTimeMillis() - start));
+        
+    }
+    
+}
