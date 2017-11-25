cdfe052dae49e194e661979ffba196ae2227abcf
Igor Salnikov <isalnikov1@gmail.com>
2017-11-25 17:32:12 +0300

    add fix

--- src/test/java/ru/isalnikov/sportloto/completablefuture/ScheduledThreadPoolTest.java
+++ src/test/java/ru/isalnikov/sportloto/completablefuture/ScheduledThreadPoolTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import org.junit.Test;
 
 /**
  * https://tproger.ru/translations/java8-concurrency-tutorial-1/
@@ -11,16 +12,24 @@ import java.util.concurrent.TimeUnit;
  */
 public class ScheduledThreadPoolTest {
 
-    public static void main(String[] args) throws InterruptedException {
+    public ScheduledThreadPoolTest() {
+    }
+
+    
+    
+     @Test
+    public void nScheduledThreadPooleTest() throws InterruptedException{
         
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
-        Runnable task = () -> System.out.println("Scheduling: " + System.nanoTime());
+        Runnable task = () -> System.out.println("Scheduling: " + System.currentTimeMillis());
+        
         ScheduledFuture<?> future = executor.schedule(task, 3, TimeUnit.SECONDS);
 
-        TimeUnit.MILLISECONDS.sleep(1337);
+        TimeUnit.MILLISECONDS.sleep(5000);
 
         long remainingDelay = future.getDelay(TimeUnit.MILLISECONDS);
-        System.out.printf("\nRemaining Delay: %s ms", remainingDelay);
+        
+        System.out.printf("\nRemaining Delay: %s ms\n", remainingDelay);
     }
 }
