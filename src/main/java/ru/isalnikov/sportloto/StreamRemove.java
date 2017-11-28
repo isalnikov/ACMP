5b5780903e7ff9d71c5d449f5d1af75aff198401
Igor Salnikov <isalnikov1@gmail.com>
2017-11-28 19:18:34 +0300

    add fix

---
+++ src/main/java/ru/isalnikov/sportloto/StreamRemove.java
@@ -0,0 +1,50 @@
+package ru.isalnikov.sportloto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Igor Salnikov igor.salnikov@stoloto.ru
+ */
+public class StreamRemove {
+
+    public static void main(String[] args) {
+
+        List<Integer> nums = new ArrayList();
+        for (int i = 1; i < 10_000_000; i++) {
+            nums.add(i);
+
+        }
+
+        List<Integer> filteredList;
+
+        long start = System.currentTimeMillis();
+        filteredList = nums.stream().filter(i -> i >= 3).collect(Collectors.toList());
+        System.out.println(filteredList.size());
+        long stop = System.currentTimeMillis();
+        System.out.println((stop - start) + " ms stream");
+
+        start = System.currentTimeMillis();
+        filteredList = nums.stream().filter(i -> i >= 3).collect(Collectors.toList());
+        System.out.println(filteredList.size());
+        stop = System.currentTimeMillis();
+        System.out.println((stop - start) + " ms stream");
+
+        start = System.currentTimeMillis();
+        filteredList = nums.parallelStream().filter(i -> i >= 3).collect(Collectors.toList());
+        stop = System.currentTimeMillis();
+        System.out.println(filteredList.size());
+
+        System.out.println((stop - start) + " ms parallelStream");
+
+        start = System.currentTimeMillis();
+        filteredList = nums.parallelStream().filter(i -> i >= 3).collect(Collectors.toList());
+        stop = System.currentTimeMillis();
+        System.out.println(filteredList.size());
+
+        System.out.println((stop - start) + " ms parallelStream");
+
+    }
+}
