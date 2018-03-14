e83116ecb3756c7adb020442f3d595a1f80acb0a
Igor Salnikov <isalnikov1@gmail.com>
2018-03-14 19:06:58 +0300

    promise

--- src/test/java/ru/isalnikov/sportloto/completablefuture/Java8Test.java
+++ src/test/java/ru/isalnikov/sportloto/completablefuture/Java8Test.java
@@ -1,6 +1,14 @@
 package ru.isalnikov.sportloto.completablefuture;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 import org.junit.Test;
 
 /**
@@ -24,19 +32,17 @@ public class Java8Test {
 
         public String firstName;
         public String lastName;
+        public String fullName;
 
         @Override
         public String toString() {
-            return  firstName + " " + lastName;
+            return firstName + " " + lastName;
         }
 
-        
-        
         public Person(String firstName, String lastName) {
             this.firstName = firstName;
             this.lastName = lastName;
-            
-            
+
         }
 
     }
@@ -46,12 +52,12 @@ public class Java8Test {
         Consumer<Person> greeter = (p) -> System.out.println("Hello, " + p.firstName);
         greeter.accept(new Person("Luke", "Skywalker"));
     }
-    
+
     @Test
     public void testPersonFactory() {
-       PersonFactory<Person> personFactory = Person::new;
-       Person person = personFactory.create("Peter", "Parker");
-       System.out.println(person);    // 123
+        PersonFactory<Person> personFactory = Person::new;
+        Person person = personFactory.create("Peter", "Parker");
+        System.out.println(person);    // 123
     }
 
     @Test
@@ -59,6 +65,46 @@ public class Java8Test {
         Converter<String, Integer> converter = (from) -> Integer.valueOf(from);
         Integer converted = converter.to("123");
         System.out.println(converted);    // 123
+    }
+
+    // https://habrahabr.ru/post/260953/
+    private static Person renamePerson(Person p) {
+        p.fullName = p.firstName + " : " + p.lastName;
+        return p;
+
+    }
+
+    @Test
+    public void testMap() {
+
+//        CompletableFuture[] futures = findPricesStream("myPhone")
+//.map(f -> f.thenAccept(System.out::println))
+//.toArray(size -> new CompletableFuture[size]);
+//CompletableFuture.allOf(futures).join();
+        final ThreadFactory threadFactory = new ThreadFactoryBuilder()
+                .setNameFormat("Поток-%d")
+                .setDaemon(true)
+                .build();
+
+        final ExecutorService es = Executors.newFixedThreadPool(2, threadFactory);
+
+        List<Person> persons = Arrays.asList(new Person("1", "2"), new Person("3", "4"));
+
+        CompletableFuture[] futures
+                = 
+                        persons.stream()
+                        .map(p -> CompletableFuture.supplyAsync(
+                        () -> renamePerson((Person) p), es))
+                        .map(f -> f.thenAccept(System.out::println))
+                        .toArray(size -> new CompletableFuture[size]);
+
+        CompletableFuture.allOf(futures).join();
+        
+        
+        
+        
+        persons.stream().forEach(p -> System.out.println(p.fullName));
+
     }
 
 }
