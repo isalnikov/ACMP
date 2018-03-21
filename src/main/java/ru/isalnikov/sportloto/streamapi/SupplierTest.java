842a854972b6ec3f5b1833c8e89bf93e0b70de25
Igor Salnikov <isalnikov1@gmail.com>
2018-03-21 18:48:14 +0300

    test

---
+++ src/main/java/ru/isalnikov/sportloto/streamapi/SupplierTest.java
@@ -0,0 +1,34 @@
+package ru.isalnikov.sportloto.streamapi;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Supplier;
+
+/**
+ *
+ * @author Igor Salnikov igor.salnikov@stoloto.ru
+ */
+public class SupplierTest 
+
+{
+    
+    
+    
+    public static void main(String[] args) {
+        Set<Supplier<Number>> set = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            final int n = i;
+            
+            set.add(()->0);
+            set.add(()->n);
+            set.add(Math::random);
+            
+            
+            
+            
+        }
+        
+        
+        
+    }
+}
