44d56ab6c1441f50ce5fd7fcd3dc8fbcdb652996
Igor Salnikov <isalnikov1@gmail.com>
2018-03-29 17:17:28 +0300

    Дан массив с целыми числами, среди которых есть нули. Сдвинуть все нули в  начало массива?

--- src/main/java/ru/isalnikov/sportloto/ZeroArray.java
+++ src/main/java/ru/isalnikov/sportloto/ZeroArray.java
@@ -1,6 +1,7 @@
 package ru.isalnikov.sportloto;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
  * Дан массив с целыми числами, среди которых есть нули. Сдвинуть все нули в
@@ -11,6 +12,13 @@ import java.util.Arrays;
 public class ZeroArray {
 
     public static void main(String[] args) {
+
+        test1();
+        System.exit(0);
+
+    }
+
+    private static void test3() {
         int[] array = Arrays.asList(1, 2, 0, 0, 3, 4, 0, 5, 1, 0, 9, 7, 0)
                 .stream()
                 .mapToInt(Integer::intValue)
@@ -25,10 +33,33 @@ public class ZeroArray {
         for (int ii = i; ii >= 0; ii--) {
             array[ii] = 0;
         }
-        
+
         for (int ii = 0; ii < array.length; ++ii) {
             System.out.print(array[ii] + " ");
         }
+    }
 
+    public static void test() {
+
+        int[] arr = {4, 0, 3, 0, 0, 6, 7, 0, 9, 0, 34, 0};
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0, j = 0; j < arr.length; j++) {
+            if (arr[j] == 0) {
+                arr[j] = arr[i];
+                arr[i] = 0;
+                i++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static void test1() {
+        int[] array = new int[]{0, 12, 234, 0, 42, 5, 0};
+
+        IntStream intStreamZero = Arrays.stream(array).filter(p -> p == 0);
+        IntStream intStreamNonZero = Arrays.stream(array).filter(p -> p != 0);
+
+        IntStream.concat(intStreamZero, intStreamNonZero).forEach(System.out::println);
     }
 }
