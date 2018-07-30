19d80ecbaf0089cf3707ac9430031c88dc6f0b61
Igor Salnikov <igor.salnikov@stoloto.ru>
2018-07-30 18:51:04 +0300

    https://ru.wikipedia.org/wiki/%D0%A8%D0%B8%D1%84%D1%80_%D0%92%D0%B8%D0%B6%D0%B5%D0%BD%D0%B5%D1%80%D0%B0

---
+++ src/main/java/ru/isalnikov/alfabank/Morpheus.java
@@ -0,0 +1,43 @@
+package ru.isalnikov.alfabank;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * 
+ * https://tjournal.ru/etc/passwd
+ 
+ * https://planetcalc.ru/2463/
+ * 
+ * чпедк ъюолт пивз кгояшнас ецвлтт чх гшнбш шэфгы, ьь вж пбъбжфшо бьызкб лаш фсзэсэже цоыэже: йуакье,еяц,якто,ььыз,дфо,ьуфн 25,95,36,17,14,49. вэъфщяь чг ьрсеоъш ф вдбнсз в ээс.
+ * 
+ * ключ : ПЛАТОН
+ * 
+ * @author Igor Salnikov igor.salnikov@stoloto.ru
+ */
+public class Morpheus {
+    public static void main(String[] args) {
+        //четыре,два,пять,ноль,один,три 19,46,58,58,50,131
+        
+      args = new String[] {"19,46,58,58,50,131","4,2,5,0,1,3"};
+        
+        
+        int[] arr1 = Arrays.asList(args[0].split(",")).stream().mapToInt(Integer::parseInt).toArray();
+        int[] arr2 = Arrays.asList(args[1].split(",")).stream().mapToInt(Integer::parseInt).toArray();
+        
+        
+        
+        Optional<String> s = Stream.iterate(0,i->arr2[i])
+                .map(res -> arr1[res])
+                .skip(1)
+                .limit(arr2.length)
+                .map(Object::toString)
+                .reduce((a,b)-> a+ ", " + b);
+        
+        if(s.isPresent()){
+         //   s.map(s -> new StringBuilder(s).reverse().toString());
+        }
+        System.out.println(String.format("%s-%s-%s-%s-%s-%s", s.get().split(",")));
+    }
+}
