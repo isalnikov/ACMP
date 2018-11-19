d79a87f5a86f34efebada380c11ac1568f738071
Igor Salnikov <igor.salnikov@stoloto.ru>
2018-11-19 14:47:42 +0300

    add link

--- src/main/java/ru/isalnikov/acmp/info/package-info.java
+++ src/main/java/ru/isalnikov/acmp/info/package-info.java
@@ -3,4 +3,6 @@ package ru.isalnikov.acmp.info;
  * 
  * https://docs.google.com/spreadsheets/d/1w5JqsXFfNTS7NUT7ujKrt8NvhJiw52S6jfvoo2X_fo8/edit#gid=0
  * 
+ * https://www.youtube.com/playlist?list=PLIEv2lyiPlJQuuDEnAPfa4iybE7GU62h4
+ * 
  */
