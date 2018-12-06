12d3ca739722c36ddb9791a576fd34457abd7efd
Igor Salnikov <igor.salnikov@stoloto.ru>
2018-12-06 16:16:37 +0300

    add reztov problem

--- src/main/java/ru/isalnikov/sportloto/reztsov/AlexeyReztsovProblem.java
+++ src/main/java/ru/isalnikov/sportloto/reztsov/AlexeyReztsovProblem.java
@@ -1,7 +1,13 @@
 package ru.isalnikov.sportloto.reztsov;
 
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Function;
+import static java.util.stream.Collectors.toList;
 import java.util.stream.Stream;
 
 /*
@@ -17,12 +23,25 @@ import java.util.stream.Stream;
  */
 public class AlexeyReztsovProblem {
 
-    public static Stream reztsov(Stream stream, Stream<Function> a2) {
-        return Stream.empty();
+    public static Stream<Integer> reztsov(Stream<Integer> stream, Stream<Function> f) {
+        
+        List<Integer> results = stream.collect(toList());
+        List<Function> list = f.collect(toList());
+        for (Function function : list) {
+            
+            List<Integer> objs = (List<Integer>) results.stream().map(function).collect(toList());
+            results  = objs;
+        }
+        
+        return results.stream();
     }
     
     public static Stream reztsov(Stream<Integer> stream, Function<Integer,Integer> a1, Function<Integer,Integer> a2) {
 
+        
+        
+        
+        
 //  /      return stream
 //                .map(a1)
 //                .collect(
@@ -36,6 +55,19 @@ return Stream.empty();
             
     
     public static void main(String[] args) {
+        
+        Stream<Integer> input = Stream.of(1, 3, 2, 5, 4);
+        
+        
+         Function<Integer,Integer> add1 = x -> x + 1;
+         Comparator<Integer> sort = (a, b) -> a - b;
+         
+        //Arrays.s
+        //ad//d1.andThen(add1)
+        Stream f = Stream.of(add1,sort);
+         
+        reztsov(input ,f).forEach(System.out::println);
+        
 //        //    IntStream stream = IntStream.of(1, 3, 2, 5, 4); 
 //         int[] intArray = {1, 3, 2, 5, 4};
 //        
@@ -46,11 +78,11 @@ return Stream.empty();
 ////                .sorted()
 ////                .reduce(op);
 //        
-        Function<Integer,Integer> add1 = x -> x + 1;
-        Function<Integer,Integer> concat = x -> x + 1;
+        //Function<Integer,Integer> add1 = x -> x + 1;
+        //Function<Integer,Integer> concat = x -> x + 1;
 
         //Function.identity().;
-      reztsov(Stream.of(1, 3, 2, 5, 4),add1,concat).forEach(System.out::println);
+      //reztsov(Stream.of(1, 3, 2, 5, 4),add1,concat).forEach(System.out::println);
       //  .filter(predicate);
     }
 }
