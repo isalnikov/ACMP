dd23ba1d032e00c70fba4532409256c6c64ad8c7
i.salnikov <i.salnikov@salnikov-i-2.orglot.office>
2019-01-16 12:16:02 +0300

    fix sol

--- src/main/java/ru/isalnikov/sportloto/Collect.java
+++ src/main/java/ru/isalnikov/sportloto/Collect.java
@@ -21,6 +21,8 @@ public class Collect {
                 StringBuilder::append,
                 StringBuilder::append).toString();
         
+        System.out.println(collect);
+        
         
 //          chars.stream().reduce(
 //                StringBuilder::new,
