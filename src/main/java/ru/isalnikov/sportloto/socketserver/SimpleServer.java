60ac12602a0f9c6054236676b00949d796c8d6ec
Igor Salnikov <igor.salnikov@stoloto.ru>
2019-01-24 17:23:21 +0300

    socketserver

---
+++ src/main/java/ru/isalnikov/sportloto/socketserver/SimpleServer.java
@@ -0,0 +1,53 @@
+package ru.isalnikov.sportloto.socketserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
+/**
+ * run $telnet localhost 8080
+ *
+ * https://www.youtube.com/watch?v=Yi_m6ju4MTQ
+ *
+ * @author Igor Salnikov igor.salnikov@stoloto.ru
+ */
+public class SimpleServer {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(8080);
+        ExecutorService service = Executors.newCachedThreadPool();
+        
+        System.out.println("SimpleServer start");
+        while (true) {
+            Socket s = ss.accept();
+            service.execute(() -> {
+
+                try {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    OutputStream out = s.getOutputStream();
+                    in.lines().forEach((line -> {
+
+                        out.write(("Echo : " + line + "\n").getBytes());
+
+                    }));
+                    s.close();
+
+                } catch (IOException e) {
+                    System.err.println(e);
+                }
+
+            });
+
+        }
+
+    }
+
+
+
+}
