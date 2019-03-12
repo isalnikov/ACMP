e3e9cabf2a90f28795b235350bb37dcdd8bc3b24
i.salnikov <i.salnikov@salnikov-i-2.orglot.office>
2019-03-12 13:40:46 +0300

    un

---
+++ src/main/java/ru/isalnikov/sportloto/collections/RemoveListNElements.java
@@ -0,0 +1,23 @@
+package ru.isalnikov.sportloto.collections;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author i.salnikov
+ */
+public class RemoveListNElements {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList(Arrays.asList(1,2,3,4,5,6,7,8));
+        Collections.shuffle(list);
+        int a1 = list.removeLast();
+        int a2 = list.removeLast();
+        int a3 = list.removeLast();
+        System.out.printf("%d %d %d\n" , a1,a2,a3);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
